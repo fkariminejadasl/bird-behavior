bug resolve: bug1:one batchnorm for all layers; bug2: gps speed normalization.

behavior/data.py
@@ -227,7 +227,10 @@ class BirdDataset(Dataset):
         self.label_ids = label_ids
         self.data = all_measurements.copy()
         # normalize gps speed
-        self.data[:, :, 3] = self.data[:, :, 3] / self.data[:, :, 3].max()
+        # self.data[:, :, 3] = self.data[:, :, 3] / self.data[:, :, 3].max()
+        self.data[:, :, 3] = (
+            self.data[:, :, 3] / 22.3012351755624
+        )  # all_measurements[..., 3].max()
         self.data = self.data.astype(np.float32)
 
         self.transform = transform
@@ -407,12 +410,12 @@ def reindex_ids(label_ids):
 
 
 all_measurements, label_ids = combine_all_data()
-label_ids = np.array(label_ids, dtype=np.int64)
+alabel_ids = np.array(label_ids, dtype=np.int64)
 agps_imus = np.empty(shape=(0, 20, 4))
 for i in range(0, 10):
-    agps_imus = np.concatenate((agps_imus, all_measurements[label_ids == i]), axis=0)
+    agps_imus = np.concatenate((agps_imus, all_measurements[alabel_ids == i]), axis=0)
 agps_imus = agps_imus.reshape(-1, 4)
-rep_labels = np.sort(np.repeat(label_ids, 20))
+rep_labels = np.sort(np.repeat(alabel_ids, 20))
 
 # agps_imus, new_ids = get_specific_labesl(
 #     all_measurements, label_ids, target_labels=[0, 2, 4, 5]
@@ -432,3 +435,13 @@ axs[0].plot(rep_labels)
 axs[1].plot(agps_imus[:, 0], "r-*", agps_imus[:, 1], "b-*", agps_imus[:, 2], "g-*")
 axs[2].plot(agps_imus[:, 3])
 plt.show(block=False)
+
+"""
+def test(model, n=10):
+    a = all_measurements[0:n].copy()
+    a[..., 3] = a[..., 3] / 22.3012351755624
+    a = torch.tensor(a.astype(np.float32)).transpose(2,1)
+    out = model(a)
+    print(torch.argmax(out, axis=1).tolist())
+    print(label_ids[0:n])
+"""

behavior/model.py
@@ -27,22 +27,29 @@ class BirdModel(nn.Module):
             mid_channels, mid_channels, kernel_size=3, padding=1
         )
         self.fc = torch.nn.Linear(mid_channels, out_channels)
-        # self.bn = torch.nn.BatchNorm1d(mid_channels)
-        self.bn = torch.nn.BatchNorm1d(mid_channels, track_running_stats=False)
+        self.bn1 = torch.nn.BatchNorm1d(mid_channels)
+        self.bn2 = torch.nn.BatchNorm1d(mid_channels)
+        self.bn3 = torch.nn.BatchNorm1d(mid_channels)
         self.relu = torch.nn.ReLU()
         self.avgpool = torch.nn.AdaptiveAvgPool1d(1)
 
+        # for m in self.modules():
+        #     if isinstance(m, torch.nn.Conv1d) and m.bias is not None:
+        #         torch.nn.init.kaiming_normal_(
+        #             m.weight, mode="fan_out", nonlinearity="relu"
+        #         )
+        #     if (
+        #         isinstance(m, (torch.nn.BatchNorm1d, torch.nn.Linear))
+        #         and m.bias is not None
+        #     ):
+        #         torch.nn.init.constant_(m.weight, 1)
+        #         torch.nn.init.constant_(m.bias, 0)
+
     def forward(self, x):
-        # x = self.relu(self.conv1(x))
-        # x = self.relu(self.conv2(x))
-        # x = self.relu(self.conv3(x))
-        x = self.relu(self.bn(self.conv1(x)))
-        x = self.relu(self.bn(self.conv2(x)))
-        x = self.relu(self.bn(self.conv3(x)))
-        # x = self.relu(self.conv1(x))
-        # x = self.conv1(x)
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.relu(self.bn3(self.conv3(x)))
         x = self.avgpool(x).flatten(1)
-        # x = self.relu(self.fc(x))
         x = self.fc(x)
         return x
 
@@ -275,45 +282,6 @@ loss = criterion(o, torch.rand(o.shape))
 loss.backward()
 print(o.shape)
 """
-
-
-class BirdModel_(nn.Module):
-    def __init__(self, in_channels, mid_channels, out_channels):
-        super().__init__()
-
-        self.conv1 = torch.nn.Conv1d(
-            in_channels, mid_channels, kernel_size=3, padding=1
-        )
-        self.conv2 = torch.nn.Conv1d(
-            mid_channels, mid_channels, kernel_size=3, padding=1
-        )
-        self.conv3 = torch.nn.Conv1d(
-            mid_channels, mid_channels, kernel_size=3, padding=1
-        )
-        self.fc = torch.nn.Linear(mid_channels, out_channels)
-        self.bn = torch.nn.BatchNorm1d(mid_channels)
-        self.relu = torch.nn.ReLU()
-        self.avgpool = torch.nn.AdaptiveAvgPool1d(1)
-
-        for m in self.modules():
-            if isinstance(m, torch.nn.Conv1d) and m.bias is not None:
-                torch.nn.init.kaiming_normal_(
-                    m.weight, mode="fan_out", nonlinearity="relu"
-                )
-            if (
-                isinstance(m, (torch.nn.BatchNorm1d, torch.nn.Linear))
-                and m.bias is not None
-            ):
-                torch.nn.init.constant_(m.weight, 1)
-                torch.nn.init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        x = self.relu(self.bn(self.conv1(x)))
-        x = self.relu(self.bn(self.conv2(x)))
-        x = self.relu(self.bn(self.conv3(x)))
-        x = self.avgpool(x).flatten(1)
-        x = self.fc(x)
-        return x
 
 
 class BirdModelTransformer_(nn.Module):