e8db9578ab91138ac5761bf9af3d53f4886ee8cf
Fatemeh Karimi Nejadasl <fkariminejadasl@gmail.com>
2023-10-18 17:32:03 +0200

    with dropout

--- behavior/model.py
+++ behavior/model.py
@@ -49,6 +49,7 @@ class BirdModel(nn.Module):
         x = self.relu(self.bn1(self.conv1(x)))
         x = self.relu(self.bn2(self.conv2(x)))
         x = self.relu(self.bn3(self.conv3(x)))
+        x = torch.nn.functional.dropout(x, p=0.25, training=True)
         x = self.avgpool(x).flatten(1)
         x = self.fc(x)
         return x
