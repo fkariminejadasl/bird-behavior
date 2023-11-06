bugfix: issue with binary ap calculation; save gps speed

behavior/data.py
@@ -354,12 +354,14 @@ print(dict(zip(range(1, 11), np.sum(hists[:, 1:], axis=0))))
 print(count)
 
 # from datetime import datetime
-# datetime.utcfromtimestamp(1402132788).strftime('%Y-%m-%d %H:%M:%S')
-# int(datetime.strptime('2014-06-07 11:19:48', '%Y-%m-%d %H:%M:%S').strftime("%s"))
+# >>> datetime.strptime('2023-11-06 14:08:11.915636', "%Y-%m-%d %H:%M:%S.%f").timestamp()
+# 1699276091.915636
+# >>> datetime.fromtimestamp(1699276091.915636).strftime("%Y-%m-%d %H:%M:%S.%f")
+# '2023-11-06 14:08:11.915636'
 """
 
 
-def combine_all_data(train_path, valid_path, test_path):
+def combine_all_data(train_path, valid_path, test_path, return_all=False):
     labels1, label_ids1, device_ids1, time_stamps1, all_measurements1 = read_data(
         train_path
     )
@@ -373,13 +375,15 @@ def combine_all_data(train_path, valid_path, test_path):
     all_measurements = np.concatenate(
         (all_measurements1, all_measurements2, all_measurements3), axis=0
     )
-    # labels = labels1 + labels2 + labels3
-    # device_ids = device_ids1 + device_ids2 + device_ids3
-    # time_stamps = time_stamps1 + time_stamps2 + time_stamps3
     inds = np.arange(all_measurements.shape[0])
     np.random.shuffle(inds)
     all_measurements = all_measurements[inds]
     label_ids = list(np.array(label_ids)[inds])
+    if return_all:
+        # labels = labels1 + labels2 + labels3
+        device_ids = device_ids1 + device_ids2 + device_ids3
+        time_stamps = time_stamps1 + time_stamps2 + time_stamps3
+        return all_measurements, label_ids, device_ids, time_stamps
     return all_measurements, label_ids
 
 

behavior/model.py
@@ -26,14 +26,14 @@ class BirdModel(nn.Module):
         self.conv3 = torch.nn.Conv1d(
             mid_channels, mid_channels, kernel_size=3, padding=1
         )
-        self.conv4 = torch.nn.Conv1d(
-            mid_channels, mid_channels, kernel_size=3, padding=1
-        )
+        # self.conv4 = torch.nn.Conv1d(
+        #     mid_channels, mid_channels, kernel_size=3, padding=1
+        # )
         self.fc = torch.nn.Linear(mid_channels, out_channels)
         self.bn1 = torch.nn.BatchNorm1d(mid_channels)
         self.bn2 = torch.nn.BatchNorm1d(mid_channels)
         self.bn3 = torch.nn.BatchNorm1d(mid_channels)
-        self.bn4 = torch.nn.BatchNorm1d(mid_channels)
+        # self.bn4 = torch.nn.BatchNorm1d(mid_channels)
         self.relu = torch.nn.ReLU()
         self.avgpool = torch.nn.AdaptiveAvgPool1d(1)
 
@@ -51,14 +51,14 @@ class BirdModel(nn.Module):
 
     def forward(self, x):
         x = self.relu(self.bn1(self.conv1(x)))
-        identity = x
+        # identity = x
         x = torch.nn.functional.dropout(x, p=0.25, training=True)
         x = self.relu(self.bn2(self.conv2(x)))
         x = torch.nn.functional.dropout(x, p=0.25, training=True)
         x = self.relu(self.bn3(self.conv3(x)))
         x = torch.nn.functional.dropout(x, p=0.25, training=True)
-        x = self.relu(self.bn4(self.conv4(x))) + identity
-        x = torch.nn.functional.dropout(x, p=0.25, training=True)
+        # x = self.relu(self.bn4(self.conv4(x))) + identity
+        # x = torch.nn.functional.dropout(x, p=0.25, training=True)
         x = self.avgpool(x).flatten(1)
         x = self.fc(x)
         return x
@@ -71,9 +71,9 @@ from behavior.transformer import MultiheadAttention, SimpleTransformer
 
 
 class BirdModelTransformer(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, out_channels) -> None:
         super().__init__()
-        out_embed_dim = 4
+        out_embed_dim = out_channels
         embed_dim = 512
         num_blocks = 1  # 6
         num_heads = 1  # 8

exps/exps1.py
@@ -21,14 +21,18 @@ torch.manual_seed(seed)
 
 
 def save_data_prediction(save_path, label, pred, conf, data):
-    rand = np.random.randint(0, 255, 1)[0]
-    name = f"label: {label}, pred: {pred}, conf: {conf:.1f}"
+    """
+    data: np.ndary
+        Lx4: L: length is usually 20
+    """
+    gps = data[0, -1]
+    name = f"label: {label}, pred: {pred}, conf: {conf:.1f}, gps: {gps:.4f}"
     _, ax = plt.subplots(1, 1)
     ax.plot(data[:, 0], "r-*", data[:, 1], "b-*", data[:, 2], "g-*")
     ax.set_xlim(0, 20)
     ax.set_ylim(-3.5, 3.5)
     plt.title(name)
-    plt.savefig(save_path / f"{name}_{rand}.png", bbox_inches="tight")
+    plt.savefig(save_path / f"{name}.png", bbox_inches="tight")
     plt.close()
 
 
@@ -82,15 +86,16 @@ ind2name = {
 }
 
 save_path = Path("/home/fatemeh/Downloads/bird/result/")
-train_per = 0.9
+train_per = 0.5
 data_per = 1
-exp = 71  # sys.argv[1]
+exp = 73  # sys.argv[1]
 width = 30
 # target_labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-target_labels = [0, 1, 2, 3, 4, 5, 6, 8, 9]  # no Other
+# target_labels = [0, 1, 2, 3, 4, 5, 6, 8, 9]  # no Other
 # target_labels = [0, 2, 3, 4, 5, 6] # no: Exflap:1, Other:7, Manauvre:8, Pecking:9
 # target_labels = [0, 3, 4, 5, 6]  # no: Exflap:1, Soar:2, Other:7, Manauvre:8, Pecking:9
 # target_labels = [0, 2, 4, 5]
+target_labels = [8, 9]
 target_labels_names = [ind2name[t] for t in target_labels]
 n_classes = len(target_labels)
 fail_path = save_path / f"failed/{exp}"
@@ -182,7 +187,10 @@ def helper_results(data, labels, stage="valid", SAVE_FAILED=False):
         labels = np.concatenate((labels, inds))
         prob = np.concatenate((prob, np.zeros((len(inds), prob.shape[1]))))
 
-    ap = average_precision_score(labels, prob)
+    if n_classes:
+        ap = average_precision_score(labels, np.argmax(prob, axis=1))
+    else:
+        ap = average_precision_score(labels, prob)
     print(ap, loss.item(), accuracy)
     print(confmat)
 
@@ -190,10 +198,11 @@ def helper_results(data, labels, stage="valid", SAVE_FAILED=False):
         inds = np.where(pred != labels)[0]
         for ind in inds:
             label_name = target_labels_names[labels[ind]]
-            pred_name = target_labels_names[pred[ind]]
-            conf = prob[ind, pred[ind]]
-            data_item = data[ind].transpose(1, 0).cpu().numpy()
-            save_data_prediction(fail_path, label_name, pred_name, conf, data_item)
+            if label_name == "Pecking":
+                pred_name = target_labels_names[pred[ind]]
+                conf = prob[ind, pred[ind]]
+                data_item = data[ind].transpose(1, 0).cpu().numpy()
+                save_data_prediction(fail_path, label_name, pred_name, conf, data_item)
 
 
 print(device)