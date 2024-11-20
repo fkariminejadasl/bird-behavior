2675b7309fb93efd960ff22e4216cd7fd0135638
fkariminejadasl <fkariminejadasl@gmail.com>
2024-11-20 17:00:54 +0100

    small change to save all

--- exps/save_plots_gt.py
+++ exps/save_plots_gt.py
@@ -16,9 +16,11 @@ df_db = pd.read_csv(
 )
 
 # Unique device and starting times
+save_all_path = gt_path / "all"
+save_all_path.mkdir(parents=True, exist_ok=True)
 df = df[df[3] != 7]
 unique_dt = df.groupby(by=[0, 1])
-for i, dt in tqdm(enumerate(unique_dt.groups.keys())):
+for i, dt in tqdm(enumerate(unique_dt.groups.keys()), total=len(unique_dt)):
     dataframe = unique_dt.get_group(dt).sort_values(by=[2])
     dataframe_db = df_db[(df_db[0] == dt[0]) & (df_db[1] == dt[1])].sort_values(by=[2])
     # print(dt[0], dt[1], len(dataframe))
@@ -29,6 +31,7 @@ for i, dt in tqdm(enumerate(unique_dt.groups.keys())):
     save_path = gt_path / f"{label}"
     save_path.mkdir(parents=True, exist_ok=True)
     fig.savefig(save_path / f"{name}.png", bbox_inches="tight")
+    fig.savefig(save_all_path / f"{name}.png", bbox_inches="tight")
     plt.close()
 
 # To test the for loop for case with 20 elements labeled and 40 in db
