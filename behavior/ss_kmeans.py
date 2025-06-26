312b57386dce358018af24f6ca2179803dac4e69
fkariminejadasl <fkariminejadasl@gmail.com>
2025-06-26 18:13:46 +0200

    fix the joblib bug in higher version of sklearn

--- behavior/ss_kmeans.py
+++ behavior/ss_kmeans.py
@@ -8,7 +8,7 @@ import random
 import numpy as np
 import torch
 from sklearn.utils import check_random_state
-from sklearn.utils._joblib import Parallel, delayed, effective_n_jobs
+from joblib import Parallel, delayed, effective_n_jobs
 from tqdm import tqdm
 
 
