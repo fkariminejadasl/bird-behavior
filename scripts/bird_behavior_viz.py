789c2535401765162f3b19bda58358f3f941e1a7
fkariminejadasl <fkariminejadasl@gmail.com>
2025-08-19 14:48:26 +0200

    visualize imu and behavior with their gr, prediction and probability

--- scripts/bird_behavior_viz.py
+++ scripts/bird_behavior_viz.py
@@ -195,6 +195,67 @@ def prepare_imu_gps_class_data(data_file, save_file, cfg):
     df.to_csv(save_file, index=False, header=None, float_format="%.6f")
 
 
+def plot_labeled_data(df, ind2name, glen=20):
+
+    y_limits = [-3.5, 3.5]
+
+    # Data
+    df = df.sort_values([0, 1, 2]).reset_index(drop=True)
+    assert len(np.unique(df[0])) == 1
+    assert len(np.unique(df[1])) == 1
+    assert len(np.unique(df[7])) == 1
+    data = df[[4, 5, 6]].values  # IMU data
+    indices = df[2].values  # Indices
+
+    # Draw IMU data
+    width = 4 * len(df) // glen + 2
+    fig, ax = plt.subplots(1, 1, figsize=(width, 4.3))
+    mgr = plt.get_current_fig_manager()
+    # # x=100, y=100 on screen
+    mgr.window.wm_geometry("+100+100")  # TkAgg
+    # mgr.window.setGeometry(100, 100, width_px, height_px) # Qt5Agg
+    ax.plot(
+        indices,
+        data[:, 0],
+        "r-*",
+        indices,
+        data[:, 1],
+        "b-*",
+        indices,
+        data[:, 2],
+        "g-*",
+    )
+
+    # Set axis limits
+    ax.set_xlim(indices[0], indices[-1] + 1)
+    ax.set_ylim(*y_limits)
+    ax.set_yticks([y_limits[0], 0, y_limits[1]])
+    ax.set_xticks(range(indices[0], indices[-1] + glen, glen))
+
+    # Draw horizontal zero line
+    ax.plot([indices[0], indices[-1]], [0, 0], "-", color="black")
+
+    # Draw vertical lines
+    for i in range(0, len(df), glen):
+        s_ind = df.iloc[i, 2]
+        e_ind = s_ind + glen
+        label_id = df.iloc[i, 3]
+        pred_id = df.iloc[i, 8]
+        prob = df.iloc[i, 9]
+        label = ind2name[label_id] if label_id != -1 else None
+        pred = ind2name[pred_id]
+        text_loc = [s_ind + (e_ind - s_ind + 1) // 4, y_limits[1] - 0.5]
+        text = f"{label}, {pred}, {prob:.3f}"
+        ax.text(*text_loc, text, color="black", fontsize=9)
+        ax.plot([s_ind, s_ind], y_limits, "-", color="black")
+    ax.plot([e_ind, e_ind], y_limits, "-", color="black")
+
+    text = f"{df.iloc[0,0]}, {df.iloc[0,1]},gps:{df.iloc[0,7]:.2f}"
+    plt.title(f"{text}", fontsize=10)  # GPS info
+    fig.tight_layout()
+    return fig
+
+
 # Prepare data
 cfg = dict(
     glen=20,
@@ -211,12 +272,13 @@ cfg = SimpleNamespace(**cfg)
 cfg.n_classes = len(cfg.labels_to_use)
 cfg.checkpoint_file = cfg.checkpoint_file / f"{cfg.exp}_best.pth"
 cfg.database_url = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@pub.e-ecology.nl:5432/eecology"
-cfg.save_path.mkdir(parents=True, exist_ok=True)
+
 
 """
 # On unlabeled data
 data_path = Path("/home/fatemeh/Downloads/bird/data/ssl/final/gull")
 save_path = Path("/home/fatemeh/Downloads/bird/data/ssl/gimu_behavior/gull")
+save_path.mkdir(parents=True, exist_ok=True)
 data_paths = sorted(list(data_path.glob("*")), key=lambda x: int(x.stem))
 for data_file in data_paths:
     device_id = int(data_file.stem)
@@ -233,18 +295,30 @@ prepare_imu_gps_class_data(data_file, save_file, cfg)
 
 
 # Visualize IMU and behavior
-gimu_beh_file = Path("/home/fatemeh/Downloads/bird/data/ssl/gimu_behavior/gull/298.csv")
+# gimu_beh_file = Path("/home/fatemeh/Downloads/bird/data/ssl/gimu_behavior/gull/298.csv")
+# dt = (298, "2010-06-07 09:43:05")
+gimu_beh_file = Path(
+    "/home/fatemeh/Downloads/bird/data/final/proc2/starts_gimu_behavior.csv"
+)
+dt = (806, "2014-05-16 12:56:53")
+dt = (6210, "2016-05-09 10:26:25")
 df = pd.read_csv(gimu_beh_file, header=None)
-dt = (298, "2010-06-07 09:43:05")
 
-unique_dt = df.groupby(by=[0, 1])
-dataframe = unique_dt.get_group(dt)
-fig = bu.plot_labeled_data(dataframe, dataframe, bu.ind2name)
-plt.title(f"{dt[0]},{dt[1]},{dataframe.iloc[0,8]:.2f}")
 
-# Visualize map
-lat, lon = df.iloc[0, [10, 11]]  # 52.00947, 4.34438
-map_image = bmap.get_centered_map_image(lat, lon, zoom=15)
-map_image.show()
+groups = df.groupby(by=[0, 1])
+dts = list(groups.groups.keys())
+# for _ in range(5):
+# i = np.random.randint(0, len(dts))
+# print(i)
+for i in [1536, 179]:
+    dt = dts[i]
+    dataframe = groups.get_group(dt)
+    fig = plot_labeled_data(dataframe, bu.ind2name)
+
+
+# # Visualize map
+# lat, lon = df.iloc[0, [10, 11]]  # 52.00947, 4.34438
+# map_image = bmap.get_centered_map_image(lat, lon, zoom=15)
+# map_image.show()
 
 print("done")
